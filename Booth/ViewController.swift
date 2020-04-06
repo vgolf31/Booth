d51d070d2ab774396477ba4bc6cb36904cfe3acf
Veertegh Minhas <vgolf31@gmail.com>
2020-04-06 16:05:39 -0400

    Initial Commit

---
+++ Booth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Booth
+//
+//  Created by Veer M on 4/6/20.
+//  Copyright © 2020 Veertegh Minhas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
